35217996decff390bf33157ec18f91b5cd4cd466
Roman Hnatiuk <hnatiukr@pm.me>
2025-12-02 23:42:57 +0100

    lib: Extend `read_input` arguments to return joined string

--- lib.py
+++ lib.py
@@ -1,5 +1,7 @@
-def read_input(input_filename: str):
+def read_input(input_filename: str, joined=False):
     buffer = open(input_filename, "r")
     input_map = map(lambda x: x.replace("\n", ""), buffer.readlines())
     input_list = list(input_map)
+    if joined:
+        return "".join(input_list)
     return input_list
