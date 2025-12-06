7c3db801f925ec9e4fbf927282436c8273e61288
Roman Hnatiuk <hnatiukr@pm.me>
2025-12-06 07:54:50 +0100

    lib: Revert `read_input_lines` arguments

--- lib.py
+++ lib.py
@@ -1,7 +1,6 @@
-def read_input(input_filename: str, joined=False):
+def read_input_lines(input_filename: str) -> list[str]:
     buffer = open(input_filename, "r")
     input_map = map(lambda x: x.replace("\n", ""), buffer.readlines())
     input_list = list(input_map)
-    if joined:
-        return "".join(input_list)
+
     return input_list
